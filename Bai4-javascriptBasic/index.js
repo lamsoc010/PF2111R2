8fe4501e67c953ebf3618b999bc862642db14b61
LamSoc <tranvinhlam01@gmail.com>
2021-12-15 21:38:07 +0700

    lam quen voi javascript

---
+++ Bai4-javascriptBasic/index.js
@@ -0,0 +1,9 @@
+let inputWidth;
+let inputHeight;
+
+inputWidth = parseInt(prompt("Nhập chiều rộng"));
+inputHeight = parseInt(prompt("NhậP chiều dài"));
+
+let area = inputWidth * inputHeight;
+alert("Diện tích là: " + area);
+
